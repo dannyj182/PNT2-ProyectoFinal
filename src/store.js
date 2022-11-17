54867d6fd92618dd8610259d1577e18a8066bf49
Martina Pereyra <1martinapereyra@gmail.com>
2022-11-17 00:10:28 -0300

    ajusto Navbar segun logueo

--- src/store.js
+++ src/store.js
@@ -5,14 +5,24 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
     state : {
-        estaLogueado : false,
-        getPelis: 'http://localhost:8080/cineort/peliculas/',
+        estaLogueado : true,
+        getPelis: 'http://localhost:8082/cineort/peliculas/',
     },
     actions : {
-        
+       desloguearse({commit}){
+            commit('desloguearse')   
+       },
+       loguearse({commit}) {
+        commit('loguearse')  
+    },
         
     },
     mutations : {
-       
+        desloguearse(state) {
+            state.estaLogueado = false
+        },
+        loguearse(state) {
+            state.estaLogueado = true
+        },
     }
 })
