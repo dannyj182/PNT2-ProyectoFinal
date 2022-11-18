2e259fb1e2152d716239994fd5686d16af11651e
Martina Pereyra <1martinapereyra@gmail.com>
2022-11-17 21:53:58 -0300

    creo componente de compra

---
+++ src/components/Compra.vue
@@ -0,0 +1,37 @@
+<template lang="html">
+
+  <section class="src-components-compra">
+    <h1>Compra Ticket</h1>
+  </section>
+
+</template>
+
+<script lang="js">
+
+  export default  {
+    name: 'src-components-compras',
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+
+
+</script>
+
+<style scoped lang="css">
+  .src-components-compra {
+
+  }
+</style>
