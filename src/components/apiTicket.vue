e4bbeed95bfeaa5729d37633b22f520d8575f8e8
Danny Alexander Jimenez Casares <dannyj182@gmail.com>
2022-11-24 18:25:51 -0300

    modificaciones en apiTicket

--- src/components/apiTicket.vue
+++ src/components/apiTicket.vue
@@ -2,22 +2,20 @@
   <section>
   <div class="jumbotron">
 
-   <p class="ocultar">{{getUltimoTicket}}</p>
+   <p class="ocultar">{{}}</p>
 
    <!-- <div class="jumbotron">
     <p>ticket : {{ultimoTicket}}</p>
    </div> -->
-
-  
    <div align="center" id="jb1" class="jumbotron">
       <h1>ULTIMO TICKET</h1>
       <p>Resumen de tickets cod: {{ mockApi }}</p>
       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMgGQ_7ZIsf2m-f__IEUIf4aN72TtAIDhBCg&usqp=CAU"
         width="300">
 
-      <p>Ticket nro : {{ ultimoTicket._id }}</p>
-      <p>Fecha : {{ ultimoTicket.fecha }}</p>
-      <p>Nro de Cliente : {{ultimoTicket.idUsuario}}</p>
+        <p>Ticket nro : {{ ultimoTicket._id }}</p>
+        <p>Fecha : {{ ultimoTicket.fecha }}</p>
+        <p>Nro de Cliente : {{ultimoTicket.idUsuario}}</p>
       <div v-for="(peli, index) in pelicula" :key="index">
         <div v-if="peli._id == ultimoTicket.idPelicula">
           <p>Nombre : {{peli.nombre}}</p>
@@ -67,75 +65,61 @@
       </table>
 
     </div> 
-
-    
   </div>
 </section>
 </template>
 
 <script>
-
-
-
 export default {
   name: 'src-components-api-ticket',
   props: [],
-  mounted() {
-
+  mounted () {
+    this.getDatos()
   },
   data() {
     return {
       url: 'http://localhost:8080/cineort/tickets',
       mock: 'https://6353835fe64783fa827433c7.mockapi.io/mock/peliculas/',
       mockApi: '',
-      
       usuarioUrl: 'http://localhost:8080/cineort/usuarios',
       peliculasUrl: 'http://localhost:8080/cineort/peliculas/',
       ultimo: 'http://localhost:8080/cineort/tickets/ultimoticket',
       historial: 'localhost:8080/cineort/tickets',
-      ultimoTicket: null,
+      ultimoTicket: this.getUltimo(),
       pelicula:[],
       listaTickets: [],
     }
   },
   methods: {
-
      async getData(){
       try {
         await this.buscarPelicula(),
         await this.getTicket(),
-      await this.getUltimo()
-      // await this.getMock()
-        
+        await this.getUltimo()
+      //await this.getMock()
       } catch (error) {
         console.log(error);
       }
-      
     },
-    
-
     convertirFecha(fecha) {
       return new Date(fecha).toLocaleString();
     },
-
     async getUltimo() {
       try {
         let { data: ticket } = await this.axios(this.$store.state.getUltimo)
-        this.ultimoTicket = ticket
+        return ticket
       } catch (error) {
         console.log('error en el getUltimo ' + error);
       }
     },
-
     async buscarPelicula() {
       try {
         let { data : pelicula } =  await this.axios.post(this.$store.state.getPeliculaPorId, this.ultimoTicket.idPelicula, { 'content-type' : 'application/json' })
-          this.pelicula = pelicula;
+        this.pelicula = pelicula
       } catch (error) {
         console.log(error);
       }
     },
-
     async getTicket() {
       try {
         let { data: tickets } = await this.axios(this.$store.state.getTickets)
@@ -149,26 +133,27 @@ export default {
         let { data: peliculas } = await this.axios(this.mock)
         this.mockApi = peliculas.codigo;
       } catch (error) {
-        console.error('Error en el getTickets ' + error.message);
+        console.error('Error en el getMock ' + error.message);
       }
     },
+    async getDatos(){
+      //await this.getUltimo()
+      await this.buscarPelicula()
+      await this.getTicket()
+    }
   },
   computed: {
-    getUltimoTicket(){
+    /* getUltimoTicket(){
       return this.getData()
-    }
+    } */
   }
 }
-
-
 </script>
 
 <style scoped lang="css">
 section{
   font-family: Verdana, Geneva, Tahoma, sans-serif
 }
-
-
 #jb1 {
   margin: 0 auto;
   color: rgb(255, 255, 255);
@@ -176,24 +161,20 @@ section{
   width: 38rem;
   height: 50rem;
 }
-
 #jb3 {
   color: antiquewhite;
   background-color: rgb(68, 68, 68);
 
 }
-
 .jumbotron{
   background-color:  rgba(0, 0, 0, 0.589);
 }
-
 #desc {
   font-size: 10px;
 }
 .ocultar{
   color: rgba(0, 0, 0, 0);
 }
-
 img{
   width: 200px;
 }
